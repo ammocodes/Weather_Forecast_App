8bfcdf3f9263559258ec6db8cb25c1e332df6300
amora92 <111129600+amora92@users.noreply.github.com>
2023-02-03 15:06:13 +0000

    Initial Commit

---
+++ starter/script.js
@@ -0,0 +1,5 @@
+// Header for the weather dashboard
+$("weather-header").css('background-color', 'blue');
+// A sidebar with a search form and a list of previously searched cities.
+// A main section with a card displaying the current weather for the selected city.
+// A card displaying the 5-day forecast for the selected city.
