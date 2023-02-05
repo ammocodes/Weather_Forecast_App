68344be124fe91af6a4d9b2fd98f85aae6ccd228
amora92 <111129600+amora92@users.noreply.github.com>
2023-02-05 20:15:38 +0000

    api success

--- starter/script.js
+++ starter/script.js
@@ -1,5 +1,40 @@
-// Header for the weather dashboard
-$("weather-header").css('background-color', 'blue');
-// A sidebar with a search form and a list of previously searched cities.
-// A main section with a card displaying the current weather for the selected city.
-// A card displaying the 5-day forecast for the selected city.
+// Initial variables.
+let apiKey = "452212a40dedef23b233ff248eddcb39";
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+let userInput = $('#search-input');
+let forecastResult = $('#forecast');
+let history = $('#history');
+let city = $('#city');
+let fiveDayForecast = $('#five-day-forecast');
+
+
+$("#search-button").on("click", function(event) {
+    event.preventDefault();
+    let city = $("#search-input").val();
+    let apiKey = "8e9760a756f0817505cdfc5d9f264228";
+    let queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
+
+    $.ajax({
+        url: queryUrl,
+        type: "GET",
+        dataType: "json",
+        success: function(data) {
+          console.log(data);
+          // process the retrieved data as needed
+          let cityName = data.name;
+          let temperature = data.main.temp;
+          let humidity = data.main.humidity;
+          let windSpeed = data.wind.speed;
+
+          $("#city").text("In " + cityName);
+          $("#temperature").text("The temp is " + temperature + "°C");
+          $("#humidity").text("Current humidity is " + humidity + "%");
+          $("#wind-speed").text("Current windspeed is " + windSpeed + "mph");
+          console.log(cityName, temperature, humidity, windSpeed);
+        },
+        error: function(error) {
+          console.log(error);
+          // handle the error if the request fails
+        }
+      });
+});
